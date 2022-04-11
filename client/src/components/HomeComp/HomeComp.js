83534db9ce2755dd3d3bdb973a02b221a1f56184
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-11 14:16:49 -0400

    changed up some UI

--- client/src/components/HomeComp/HomeComp.js
+++ client/src/components/HomeComp/HomeComp.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './homeComp.css';
-import PestHeader from './pest-header.jpg';
-import CarpAnt from './carp-ant.png';
-import Ants from './ants.png';
-import Mouse from './mouse.png';
-import Bird from './bird.png';
-import BedBug from './bedbug.png';
-import Tick from './tick.png';
-import KillBugs from './kill-bugs.jpg';
+// import PestHeader from './pest-header.jpg';
+import CarpAnt from './ant.jpg';
+import Fly from './fly.jpg'
+import Mouse from './rat.jpg';
+import Bird from './bird.jpg';
+import BedBug from './bedbug.jpg';
+import Tick from './tick.jpg';
+import Pest from '../NavComp/pest.png'
 
 const HomeComp = () => {
 
@@ -26,10 +26,30 @@ const HomeComp = () => {
                 <div className='header-info'>
                     <h1 className='header-title'>Affordable And Reliable Pest Control Services</h1>
                     <br />
-                    <p className='header-text'>We guarantee our pest control methods are the most thorough you have ever experienced! Our customized plans are designed specifically depending upon what kind of pest you are currently dealing with, to get them out of your home and keep them out year-round.</p>
+                    <p className='header-text'>For more than 90 years, we've protected what's most valuable to you, with local specialists who go above and beyond to fix your problem and services designed to make your life easier, 365 days a year.</p>
                 </div>
                 <div className='header-img-container'>
-                    <img className='header-img' src={PestHeader} alt="pest header" />
+                    {/* <img className='header-img' src={PestHeader} alt="pest header" /> */}
+                    <div className='pest-form'>
+                        <h2 className='pest-form-title'>Get An Estimate</h2>
+                        <div className='line'></div>
+                        <label htmlFor="first">First Name</label>
+                        <input className='pest-form-inputs' type="text" name="first" id="first" placeholder='First Name' />
+
+                        <label htmlFor="last">Last Name</label>
+                        <input className='pest-form-inputs' type="text" name="last" id="last" placeholder='Last Name' />
+
+                        <label htmlFor="phone">Phone Number</label>
+                        <input className='pest-form-inputs' type="number" name="phone" id="phone" placeholder='Phone Number' />
+
+                        <label htmlFor="email">Email Address</label>
+                        <input className='pest-form-inputs' type="email" name="email" id="email" placeholder='Email Address' />
+
+                        <label htmlFor="zip">Zipcode</label>
+                        <input className='pest-form-inputs' type="number" name="zip" id="zip" placeholder='Zipcode' />
+                        <br />
+                        <button className='form-btn' onClick={handleFormSubmit}>Submit</button>
+                    </div>
                 </div>
             </div>
 
@@ -43,6 +63,7 @@ const HomeComp = () => {
 
             <div id="our-services">
                 <h1 className='our-services-title'>Our Pest Control Services</h1>
+                <p className='our-services-subtitle'>Include the capture or elimination of:</p>
                 <div className='services-container'>
                     <div className='services'>
                         <br />
@@ -52,7 +73,7 @@ const HomeComp = () => {
                     <div className='services'>
                         <br />
                         <h5>Common Household Pests</h5>
-                        <img className='service-img' src={Ants} alt="ants" />
+                        <img className='service-img' src={Fly} alt="ants" />
                     </div>
                     <div className='services'>
                         <br />
@@ -79,17 +100,18 @@ const HomeComp = () => {
 
             <div className='servicing-container'>
                 <div className='servicing-info'>
-                    <h1>Servicing Homes and Businesses to Prevent, Repel and Eleminate Any and All Pests to Give you Peace of Mind</h1>
+                    <h1>Our service works on your schedule 24/7 customer support online inspection service Scheduling text alerts when we are on-our-way</h1>
                     <br />
-                    <p>We are licensed professionals using the very best and safest bug sprays and repellents with proven efficiency. We value each and every customer and have the experience you can trust.</p>
+                    <p>We'll come back between treatments with no additional  fees to you. This followup service is free. We guarantee we'll resolve your pest control problem. Your time and pest problems are our first priority. Once you request an inspection, we respect your busy schedule and will set up an appointment as fast as possible.</p>
+                    <br />
                     <h3>Call Us Today</h3>
                     <a href='tel:555-555-5555'><h1>555-555-5555</h1></a>
                     <br />
                 </div>
                 <br />
-                <div>
-                    <img className='servicing-img' src={KillBugs} alt="bug spray" />
-                </div>
+                {/* <div>
+                    <img className='servicing-img' src='' alt="bug spray" />
+                </div> */}
             </div>
 
             {/* <div className='axad-form'>
@@ -98,28 +120,17 @@ const HomeComp = () => {
                 <br />
                 <iframe title='form' src="https://forms.monday.com/forms/embed/b046ae4b7e6c1329190c7614ce28b296?r=use1" width="80%" height="1800" style={{ maxWidth: '1000px', border: '0', boxShadow: '5px 5px 56px 0px rgba(0,0,0,0.25)' }}></iframe>
             </div> */}
-            <div className='pest-form'>
-                <h2 className='pest-form-title'>Get An Estimate</h2>
-                <div className='line'></div>
-                <label htmlFor="first">First Name</label>
-                <input className='pest-form-inputs' type="text" name="first" id="first" placeholder='First Name' />
 
-                <label htmlFor="last">Last Name</label>
-                <input className='pest-form-inputs' type="text" name="last" id="last" placeholder='Last Name' />
-
-                <label htmlFor="phone">Phone Number</label>
-                <input className='pest-form-inputs' type="number" name="phone" id="phone" placeholder='Phone Number' />
-
-                <label htmlFor="email">Email Address</label>
-                <input className='pest-form-inputs' type="email" name="email" id="email" placeholder='Email Address' />
-
-                <label htmlFor="zip">Zipcode</label>
-                <input className='pest-form-inputs' type="number" name="zip" id="zip" placeholder='Zipcode' />
-                <br />
-                <button className='form-btn' onClick={handleFormSubmit}>Submit</button>
+            <div className='pest-man-bg'>
+                <div className='pest-man-container'>
+                    <div className='pest-man-img-container'>
+                        <img className='pest-man' src={Pest} alt="pest" />
+                    </div>
+                    <div className='pest-man-info'>
+                        <h4>Our experts will remove your pests for you! We fix several pest control nightmares - guaranteed - our expert team uses the most advanced high tech products on the market. Many of these products were invented in our own lab by staff members.</h4>
+                    </div>
+                </div>
             </div>
-            <br />
-            <br />
         </div>
     )
 }
